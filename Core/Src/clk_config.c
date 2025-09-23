558dd66a2ae1072a6c67673ac19cac27bea2d177
fuzzygreenblurs <akhil.sankar@gmail.com>
2025-09-22 20:12:32 -0400

    setting up uart. works with minicom. note: APB1 must be divided down to 42mhz if PLL = 168mhz

--- Core/Src/clk_config.c
+++ Core/Src/clk_config.c
@@ -2,14 +2,14 @@
 #include "clk_config.h"
 
 void configure_sysclk() {
-  FLASH->ACR = 5;                                                        // flash memory cannot repond fast enough to 168mhz. 
+  FLASH->ACR = 5;                                                        // flash memory cannot repond fast enough to 168mhz.
   RCC->CR |= RCC_CR_HSEON;                                               // enable the HSE clock peripheral
   while(!(RCC->CR & RCC_CR_HSERDY));                                     // wait until HSE is fully ready/non-busy
-  
-  RCC->CR &= ~RCC_CR_PLLON;                                              // disable PLL before configuration 
+
+  RCC->CR &= ~RCC_CR_PLLON;                                              // disable PLL before configuration
 
   // hardcoded to generate 168mhz (needs to be generalized)
-  RCC->PLLCFGR = 
+  RCC->PLLCFGR =
     RCC_PLLCFGR_PLLSRC_HSE |
     RCC_PLLCFGR_PLLM_3     |                                             // PLLM (constrains VCO input between 1-2mhz)
     RCC_PLLCFGR_PLLN_4     | RCC_PLLCFGR_PLLN_6 | RCC_PLLCFGR_PLLN_8;    // PLLN (set to 336 ie. double the desired 168mhz)
@@ -18,10 +18,14 @@ void configure_sysclk() {
   RCC->CR |= RCC_CR_PLLON;                                               // enable PLL and wait until full ready/non-busy
   while(!(RCC->CR & RCC_CR_PLLRDY));
 
+  // Configure bus prescalers BEFORE switching to PLL
+  RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;                                      // APB1 = SYSCLK/4 = 42MHz (max for APB1 is 42MHz)
+  RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;                                      // APB2 = SYSCLK/2 = 84MHz (max for APB2 is 84MHz)
+
   RCC->CFGR |= RCC_CFGR_SW_PLL;                                          // switch SYSCLK to pll
   while(((RCC->CFGR >> 2) & 0x03) != 2);                                 // wait until SYSCLK is fully ready (TODO: explain this logic)
 //
-//                                                                         // delay CPU by 5 cycles between fetches 
+//                                                                         // delay CPU by 5 cycles between fetches
 
 }
 
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,30 +3,89 @@
 
 #define TICKS_PER_MS 21000
 
+void configure_uart() {
+  RCC->AHB1ENR |= 1;
+  RCC->APB1ENR |= 0x20000;
+
+  GPIOA->AFR[0] &= ~0x0F00;
+  GPIOA->AFR[0] |=  0x0700;
+  GPIOA->MODER  &= ~0x0030;
+  GPIOA->MODER  |=  0x0020;
+
+  USART2->BRR  = 4375;  // 42MHz / 9600 = 4375 (when PLL enabled)
+  USART2->CR1  = 0x0008;
+  USART2->CR2  = 0x0000;
+  USART2->CR3  = 0x0000;
+  USART2->CR1 |= 0x2000;
+}
+
+
+//void configure_uart() {
+//  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;     // powers GPIOA port peripheral (pin control, alt function routing)
+//  RCC->APB1ENR |= RCC_APB1ENR_USART2EN;    // powers USART2 peripheral logic
+//                                           // enabling this clock will enable power to gpio port A.
+//
+//  GPIOA->AFR[0] &= ~0x0f00;
+//  GPIOA->AFR[0] |= 0x0700;                  // set PA2 to AF7 (USART2, see datasheet, pg 57)
+//    
+//  // USART2 uses GPIOA: PA2 and PA3 pins under AF mode
+//  GPIOA->MODER &= ~GPIO_MODER_MODER2;      // clear PA2 mode bits and set to AF mode (10)
+//  GPIOA->MODER |= GPIO_MODER_MODER2_1;
+//
+//
+//  USART2->BRR = 0x0683;                    // APB clock running at 42MHz
+//                                           // desired baud rate is 115200: BRR = 42M/115200
+//                                           // transmit/read a bit every 365 ticks, enforced by HW counter
+//
+//  USART2->CR1 = 0x0008;                   // enable tx, 8-bit data 
+//  USART2->CR2 = 0x0000;                   // 1 stop bit 
+//  USART2->CR3 = 0x0000;                   // no flow control
+//  USART2->CR1 = 0X2000;                   // enable USART2 
+//}
+
+void uart_send_char(int c) {
+  while(!(USART2->SR & 0x0080));     // wait until TX buffer is empty
+  USART2->DR = (c & 0xFF);
+}
+void delay(int n) {
+  for(; n > 0; n--) {
+    for(volatile int i = 0; i < 2000; i++);
+  }
+}
+
 void enable_onboard_led() {
-  RCC->AHB1ENR  |= RCC_AHB1ENR_GPIOAEN;    // enabling this clock will enable power to gpio port A.
   GPIOA->MODER  |= GPIO_MODER_MODER5_0;    // sets pa5 to general purpose output mode
-  GPIOA->OTYPER &= ~GPIO_OTYPER_OT_5;      // sets pa5 to push-pull output mode: equivalent to &= ~(1 << 5); 
-  GPIOA->ODR    |= (1 << 5);               // sets the pa5 bit on the output data register (ODR) to turn on LED 
+  GPIOA->OTYPER &= ~GPIO_OTYPER_OT_5;      // sets pa5 to push-pull output mode: equivalent to &= ~(1 << 5);
+  GPIOA->ODR    |= (1 << 5);               // sets the pa5 bit on the output data register (ODR) to turn on LED
 }
 
 void toggle_led(uint8_t times, uint32_t ms) {
   while(times > 0) {
     GPIOA->ODR ^= (1 << 5);
-    for(uint32_t i = 0; i < (ms * TICKS_PER_MS); i++) {
-      __asm("nop");
-    }
+//    for(uint32_t i = 0; i < (ms * TICKS_PER_MS); i++) {
+//      __asm("nop");
+//    }
 
     --times;
   }
 }
 
+void uart_send_string(const char* str) {
+  while(*str) {
+    uart_send_char(*str++);
+  }
+}
+
+
 int main(void)
 {
-  enable_onboard_led();
-  GPIOA->ODR &= ~(1 << 5);
   configure_sysclk();
-  toggle_led(200, 100);
-  while(1);
+  configure_uart();
+
+  while(1){
+    uart_send_string("hello_world, ");
+    uart_send_string("my name is fuzzy\r\n");
+    delay(1000);
+  }
 }
 
